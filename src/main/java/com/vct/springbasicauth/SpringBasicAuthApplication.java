d7ac9f5d50358341cc74cd736bff9d9691dc5a87
Victor Dantas <victorr.dantas@gmail.com>
2024-01-22 13:20:39 -0400

    Adicionando Basic Auth com Spring Security

---
+++ src/main/java/com/vct/springbasicauth/SpringBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vct.springbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vct/springbasicauth/SpringBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vct.springbasicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
